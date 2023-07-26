62d0ebfe992777218633ce5bcef545fc4f1d9c6f
ksm1569 <tnals1564@naver.com>
2023-07-26 15:59:28 +0900

    accessToken, refreshToken 발급 / tokenClaims 확인

--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -6,7 +6,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "business exception test")
+    TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "business exception test"),
+
+
+    // 인증영역
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-001", "토큰 만료"),
+    NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "A-002", "유효한 토큰이 아닙니다")
+
     ;
 
     ErrorCode(HttpStatus httpStatus, String errorCode, String message){
