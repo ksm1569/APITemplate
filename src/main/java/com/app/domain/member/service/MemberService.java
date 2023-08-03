64adcfdc296cae9b46d6f0d065bbca23c91769b1
ksm1569 <tnals1564@naver.com>
2023-08-03 20:55:56 +0900

    refresh토큰 만료테스트 완료

--- src/main/java/com/app/domain/member/service/MemberService.java
+++ src/main/java/com/app/domain/member/service/MemberService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -41,7 +42,11 @@ public class MemberService {
         Member member = memberRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new AuthenticationException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
 
-        //todo
+        LocalDateTime tokenExpirationTime = member.getTokenExpirationTime();
+
+        if(tokenExpirationTime.isBefore(LocalDateTime.now())) {
+            throw new AuthenticationException(ErrorCode.REFRESH_TOKEN_EXPIRED);
+        }
 
         return member;
     }
--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
     NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-003", "Authorization Header Null"),
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004", "인증 타입이 Bearer 타입이 아닙니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-005", "해당 REFRESH TOKEN은 존재하지 않습니다."),
-
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-006", "해당 REFRESH TOKEN은 만료되었습니다."),
     // 회원
     INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001", "잘못된 회원 타입 입니다."),
     ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입 된 회원 입니다."),
