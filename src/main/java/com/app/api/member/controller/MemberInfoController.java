ee5d65f48bb7f474a0b079052709fa4cfd0982bd
ksm1569 <tnals1564@naver.com>
2023-08-09 20:34:25 +0900

    인증 인터셉터 회원정보조회

---
+++ src/main/java/com/app/api/member/controller/MemberInfoController.java
@@ -0,0 +1,33 @@
+package com.app.api.member.controller;
+
+import com.app.api.member.dto.MemberInfoResponseDto;
+import com.app.api.member.service.MemberInfoService;
+import com.app.global.jwt.service.TokenManager;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberInfoController {
+    private final TokenManager tokenManager;
+    private final MemberInfoService memberInfoService;
+
+    @GetMapping("/info")
+    public ResponseEntity<MemberInfoResponseDto> getMemberInfo(
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        String accessToken = authorizationHeader.split(" ")[1];
+        Claims tokenClaims = tokenManager.getTokenClaims(accessToken);
+        Long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
+
+        MemberInfoResponseDto memberInfoResponseDto = memberInfoService.getMemberInfo(memberId);
+
+        return ResponseEntity.ok(memberInfoResponseDto);
+    }
+}
